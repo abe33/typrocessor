4b99d5d42565439226280d5670ab6b9945f051eb
abe33 <cedric.nehemie@gmail.com>
2016-06-15 10:09:22 +0200

    :wrench: Add spec config in spec helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,14 @@
 require 'typrocessor'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  # config.formatter = :documentation # :progress, :html, :textmate
+
+  config.order = :random
+end
