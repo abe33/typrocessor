ecf40fa7304c5e28a8b88ab75678ee75bfe60a5a
abe33 <cedric.nehemie@gmail.com>
2016-06-28 16:09:24 +0200

    Add rules for common mathematical symbols replacement

--- lib/typrocessor/replaces/math.rb
+++ lib/typrocessor/replaces/math.rb
@@ -1,6 +1,19 @@
 module Typrocessor::Replace::Math
   extend Typrocessor::RulesetMaker
 
+  Symbols = ruleset do
+    replace 'minus sign', /-/, "\u2212"
+    replace 'multiply sign', /\*/, "\u00d7"
+    replace 'multiply sign', /(\d\s*)x(\s*\d)/, "\\1\u00d7\\2"
+    replace 'divide sign', /\//, "\u00f7"
+    replace 'greater than or equal to sign', />=/, "\u2265"
+    replace 'lower than or equal to sign', /<=/, "\u2264"
+    replace 'strictly equivalent sign', /===/, "\u2263"
+    replace 'not equivalent sign', /!==/, "\u2262"
+    replace 'equivalent sign', /==/, "\u2261"
+    replace 'not equal sign', /!=/, "\u2260"
+  end
+
   Fractions = ruleset do
     Typrocessor::Constants::FRACTIONS.each do |a,b,char|
       replace "#{a}/#{b}", /\b#{a}\s*\/\s*#{b}\b/, char
--- spec/typrocessor/replaces/math_spec.rb
+++ spec/typrocessor/replaces/math_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
+describe 'Typrocessor::Replace::Math::Symbols' do
+  let(:processor) { Typrocessor::Processor.new(options) }
+  let(:options) do { rules: [Typrocessor::Replace::Math::Symbols] } end
+
+  sign_replacement = {
+    'x' => "\u00d7",
+    '*' => "\u00d7",
+    '/' => "\u00f7",
+    '-' => "\u2212",
+    '!=' => "\u2260",
+    '==' => "\u2261",
+    '!==' => "\u2262",
+    '===' => "\u2263",
+    '<=' => "\u2264",
+    '>=' => "\u2265",
+  }
+
+  sign_replacement.each_pair do |str, sign|
+    it "replaces #{str} with #{sign}" do
+      expect(processor.process("7 #{str} 10")).to eq("7 #{sign} 10")
+    end
+  end
+end
+
 describe 'Typrocessor::Replace::Math::Fractions' do
   let(:processor) { Typrocessor::Processor.new(options) }
   let(:options) do { rules: [Typrocessor::Replace::Math::Fractions] } end
