ef740d12c3d9040a10400cac805685ba098a3dee
abe33 <cedric.nehemie@gmail.com>
2016-06-28 15:10:09 +0200

    Add rules from primes replacements in math expressions

--- lib/typrocessor/replaces/math.rb
+++ lib/typrocessor/replaces/math.rb
@@ -6,4 +6,11 @@ module Typrocessor::Replace::Math
       replace "#{a}/#{b}", /\b#{a}\s*\/\s*#{b}\b/, char
     end
   end
+
+  Primes = ruleset do
+    replace 'quadruple prime', /""|''''/, "\u2057"
+    replace 'triple prime', /'''/, "\u2034"
+    replace 'double prime', /"|''/, "\u2033"
+    replace 'single prime', /'/, "\u2032"
+  end
 end
--- spec/typrocessor/replaces/math_spec.rb
+++ spec/typrocessor/replaces/math_spec.rb
@@ -11,3 +11,23 @@ describe 'Typrocessor::Replace::Math::Fractions' do
     end
   end
 end
+
+describe 'Typrocessor::Replace::Math::Primes' do
+  let(:processor) { Typrocessor::Processor.new(options) }
+  let(:options) do { rules: [Typrocessor::Replace::Math::Primes] } end
+
+  it 'replaces quotes by primes' do
+    expect(processor.process("5'6\"")).to eq("5\u20326\u2033")
+
+    expect(processor.process("x' / x\"")).to eq("x\u2032 / x\u2033")
+    expect(processor.process("a' + c\"")).to eq("a\u2032 + c\u2033")
+
+    expect(processor.process("f\"")).to eq("f\u2033")
+    expect(processor.process("f\''")).to eq("f\u2033")
+
+    expect(processor.process("d\''' - e")).to eq("d\u2034 - e")
+
+    expect(processor.process("d\'''' - e")).to eq("d\u2057 - e")
+    expect(processor.process("d\"\" - e")).to eq("d\u2057 - e")
+  end
+end
