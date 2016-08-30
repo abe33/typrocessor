5d0cef86631668781473bb1ef28004a7d247d7cc
abe33 <cedric.nehemie@gmail.com>
2016-08-30 10:23:06 +0200

    Add ignore rule for markdown strong tags

--- lib/typrocessor/ignores/markdown.rb
+++ lib/typrocessor/ignores/markdown.rb
@@ -7,6 +7,8 @@ class Typrocessor::Ignore
     ignore 'code block', /(```)(.|\n)*?\1/m
     ignore 'preformatted block', /^\x20{4}.*$/
     ignore 'inline code', /(`{1,2}).*?\1/
+    ignore 'strong', /\*\*|__/
+
     ignore 'urls', /\b((?:[a-zA-Z][\w-]+:(?:\/{1,3}|[a-zA-Z0-9%])|www\d{0,3}[.]|[a-zA-Z0-9.\-]+[.][a-zA-Z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?\u00AB\u00BB\u201C\u201D\u2018\u2019]))/
   end
 end
